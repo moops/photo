4a8caf56cd738ca2ee13bfe42afd994b93f8ac89
moops <work@raceweb.ca>
2010-11-10 11:16:42 -0800

    update TODO'

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -106,7 +106,7 @@ class Photo < ActiveRecord::Base
   private
   
   def bucket_name
-    'raceweb_photo' # << (Rails.env.production? ? '' : '_' << Rails.env)
+    'raceweb_photo' << (Rails.env.production? ? '' : '_' << Rails.env)
   end
   
 end
