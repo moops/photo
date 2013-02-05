462b51e70960d21605f00d4dd34518b4b8b92797
Adam <adam@raceweb.ca>
2013-02-04 21:50:49 -0800

    replace jdpicker with bootstrap-datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,46 +3,16 @@
 //= require jquery_ui
 //= require jquery-fileupload
 //= require bootstrap
-//= require jdpicker
-//= require jeditable
 //= require galleriffic
 //= require opacityrollover
+//= require bootstrap-datepicker
 //= require_tree .
 
 $(document).ready(function() { 
 	// calendars
-	$('#gallery_on').jdPicker();
-	$('#gallery_gallery_on').jdPicker();
-//	$('#new_photo').fileupload();
-	
-	//upload one or many photos
-	function toggle_photo_form() {
-		$('#one-photo').toggle('blind', {}, 500 );
-		$('#bunch-photo').toggle('blind', {}, 500 );
-	}
-
-	$('#btn-one-photo').click(
-		function(e) {
-			if (!$('#btn-one-photo').hasClass('active')) {
-				toggle_photo_form();
-			}
-		});
-	$('#btn-bunch-photo').click(
-		function(e) {
-			if (!$('#btn-bunch-photo').hasClass('active')) {
-				toggle_photo_form();
-			}
-		});
+	$('#gallery_gallery_on').datepicker();
 });
 
 function remove_field(element, item) {
   element.up(item).remove();
 }
-
-function showAddPhotos(id) {
-	var el = $(id);
-	if (!el.visible()) {
-		Effect.BlindDown(id, { duration: 0.5 });
-	}
-}
-
