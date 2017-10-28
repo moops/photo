1a4bcafcac6dd94526f65034cadd253a31ebbe36
Adam Lawrence <adam@raceweb.ca>
2017-10-28 12:17:54 -0700

    production s3 creds

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,16 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     Rails.logger.info('production: using fog for carrierwave storage')
-    config.storage = :fog
+    puts "s3_key: #{ENV['S3_KEY']}, s3_secret: #{ENV['S3_SECRET']}"
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_KEY'],
       aws_secret_access_key: ENV['S3_SECRET'],
-      endpoint: 'https://s3.amazonaws.com'
+      region: 'us-west-2'
     }
+    config.fog_provider = 'fog/aws'
     config.fog_directory = 'raceweb.photo'
+    config.storage = :fog
   end
   if Rails.env.development?
     Rails.logger.info('development: using file for carrierwave storage')
