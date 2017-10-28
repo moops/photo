ac6131130b708c6ad7bc99e6edb608a848d8d463
Adam Lawrence <adam@raceweb.ca>
2017-10-27 18:17:39 -0700

    load env from config/application.yml

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,20 @@ module Photos
       g.helper = false
     end
 
+    # load env variables from config/application.yml
+    config.before_configuration do
+      env_file = Rails.root.join('config', 'application.yml')
+      if File.exist?(env_file)
+        YAML.safe_load(File.open(env_file)).each do |env, keys|
+          next unless Rails.env == env
+          keys.each do |key, value|
+            # puts "#{Rails.env}: #{env}, #{key} = #{value}"
+            ENV[key.to_s] = value
+          end
+        end
+      end
+    end
+
     # S3_CONFIG = YAML::load(File.open("#{Rails.root}/config/amazon_s3.yml"))
     config.time_zone = 'Pacific Time (US & Canada)'
     DATE_FORMAT = '%B %d %Y'.freeze
