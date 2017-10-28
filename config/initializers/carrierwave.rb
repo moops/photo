ac35e5fa78d2347ea5df892d883e96e62e4e447a
Adam Lawrence <adam@raceweb.ca>
2017-10-28 12:47:10 -0700

    more fog/aws adjustments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,18 @@
-require 'fog/aws'
 CarrierWave.configure do |config|
   if Rails.env.production?
     Rails.logger.info('production: using fog for carrierwave storage')
     puts "s3_key: #{ENV['S3_KEY']}, s3_secret: #{ENV['S3_SECRET']}"
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_KEY'],
       aws_secret_access_key: ENV['S3_SECRET'],
       region: 'us-west-2'
     }
-    config.fog_provider = 'fog/aws'
-    config.fog_directory = 'raceweb.photo'
+    config.fog_attributes = {
+      'x-amz-storage-class' => 'REDUCED_REDUNDANCY'
+    }
+    config.fog_directory = 'raceweb-photo'
     config.storage = :fog
   end
   if Rails.env.development?
